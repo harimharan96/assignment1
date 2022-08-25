f3270bd9bbd678f5288413a8d26f505342ffa54a
harimharan96 <harimharan96@gmail.com>
2022-08-26 02:53:41 +0530

    commiting assignment

---
+++ script.js
@@ -0,0 +1,26 @@
+let slides = document.getElementById("slideImages");
+function btn1() {
+    slides.style.marginLeft = "0px"
+    slides.style.transition = "all 1s"
+
+}
+function btn2() {
+    slides.style.marginLeft = "-30%"
+    slides.style.transition = "all 1s"
+
+}
+function btn3() {
+    slides.style.marginLeft = "-60%"
+    slides.style.transition = "all 1s"
+
+}
+
+let schedule = document.getElementById("page3Body1");
+function slide1() {
+    schedule.style.marginLeft = "-740px"
+    schedule.style.transition = "all 1s"
+}
+function slide() {
+    schedule.style.marginLeft = "860px"
+    schedule.style.transition = "all 1s"
+}
